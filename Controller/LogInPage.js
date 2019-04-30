4318c1ca70aec99eaa2352860235bf44ae08ca55
Patrick <Patrick@wireless-169-228-81-203.ucsd.edu>
2019-04-29 21:29:27 -0700

    initialized MVC model

---
+++ Controller/LogInPage.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Text, StatusBar, Button, Alert } from 'react-native';
+import GoogleSignInButton from '../View/GoogleSignInButton';
+import firebase from 'firebase';
+import AppNavigator from './Navigation';
+
+
+var config = {
+  apiKey: "AIzaSyAXbyPsfZYy9zoNaUaSGJP6Zg1T5mCE8os",
+  authDomain: "project-gary.firebaseapp.com",
+  databaseURL: "https://project-gary.firebaseio.com",
+  projectId: "project-gary",
+  storageBucket: "project-gary.appspot.com",
+  messagingSenderId: "279978428336"
+};
+firebase.initializeApp(config);
+
+
+
+export default class LogInPage extends React.Component{
+
+
+	state = {
+    userEmail: null, 
+    userName: null,
+    isUcsd: false
+  }
+  
+  // Log in with Google. 
+  googleLogin = async () => {
+    try {
+      const result = await Expo.Google.logInAsync({
+        // Client IDs, needed to be created on Google Developers Console. 
+        //androidClientId: "Your Client ID",
+        iosClientId: "279978428336-1qa6aneofkllp8crnlh5gsn7vngo3r8q.apps.googleusercontent.com", 
+        scopes: ["profile", "email"]
+      })
+
+      if (result.type === "success") {
+        this.setState({
+          userEmail: result.user.email, 
+          userName: result.user.displayName, 
+          isUcsd: result.user.email.endsWith("@ucsd.edu")
+        })
+
+        // If user is a UCSD user, also log into firebase to access data. 
+        if (this.state.isUcsd) {
+          const credential = firebase.auth.GoogleAuthProvider.credential(result.idToken, result.accessToken);
+          firebase.auth().signInAndRetrieveDataWithCredential(credential).then(function(result){
+            //console.log(result);
+          });
+        }
+        //this.props.navigation.navigate('Where you want to go');
+      } else {
+        console.log("log in cancelled")
+      }
+      } catch (e) {
+        console.log("log in error", e)
+      }
+  }
+
+  componentDidMount() {
+    // Set up notification for logging into firebase. 
+    this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+      /*this.setState({
+        loading: false,
+        user: user,
+      });*/
+    });
+  }
+
+  componentWillUnmount() {
+    // Remove notification for logging into firebase. 
+    this.authSubscription();
+  }
+
+
+  render() {
+    const { navigate } = this.props.navigation;
+    const showAlert = () => {
+      Alert.alert(
+        'Unauthorized Account',
+        'The account you attempt to login with was not an UCSD registered account',
+        [
+          {text: 'Okay', onPress: ()=> console.log('OK Pressed')}
+        ],
+          {cancelable: false},
+      )
+    }
+		let btn;
+	    if (this.state.userEmail) {
+        {this.state.isUcsd? navigate('ProfilePage') : showAlert() }
+        btn = <GoogleSignInButton onPress={() => this.googleLogin()} /> 
+	    } else {
+	      btn = <GoogleSignInButton onPress={() => this.googleLogin()} />
+	    }
+		return(
+			<View style={styles.logInPage}>
+				<StatusBar backgroundColor="#34a9e1"/>
+				<Image source={require('./appLogo.png')}/> 
+				{btn}
+
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	logInPage: {
+		flex:1,
+		backgroundColor: '#34a9e1',
+		alignItems: 'center',
+		justifyContent: 'center',
+	
+	}
+});
+
---
+++ node_modules/.staging/react-native-google-signin-bcc62e98/.github/ISSUE_TEMPLATE/Custom.md
@@ -0,0 +1,40 @@
+---
+name: New issue template
+about: Template for new issues, any issues not following these templates will be automatically
+  closed
+---
+
+<!--
+Please make sure you have searched previous issues before opening a new issue.
+-->
+
+<!-- Describe your issue in detail. -->
+
+## Steps to Reproduce
+
+- You must provide a **minimal and easy-to-run** reproduction of your issue - [how to create a minimal reproduction?](https://stackoverflow.com/help/mcve). We're a small team of maintainers and do not have time to try reproduce bugs ourselves.
+
+- Please try to reproduce the bugs on the provided example app. Either provide a link to the repo that reproduces the bug or provide your version of the [`index.js` file](https://github.com/react-native-community/react-native-google-signin/blob/master/example/index.js) that reproduces the issue and that we can easily use.
+
+- Explain the steps we need to take to reproduce the issue.
+
+- Include a gif if you think it may help.
+
+- If you're asking for help, please make it easy for us to help you!
+
+## Expected Behavior
+
+<!-- Write what you thought would happen. -->
+
+## Actual Behavior
+
+<!-- Write what happened. Include a gif if needed. If this is a regression, let us know. -->
+
+## Environment
+
+Please provide the version of your
+
+- react-native
+- react-native-google-signin
+- cocoapods and GoogleSignIn pod version (if applicable)
+- version of Android, gradle, android plugin for gradle, `play-services-auth` and version of the Google Play Services app on your phone (if applicable)
