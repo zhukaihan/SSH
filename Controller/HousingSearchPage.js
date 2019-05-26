2b30dc59341766a7fe45739d5108e119c977edd1
YuWang711 <45251434+YuWang711@users.noreply.github.com>
2019-05-25 21:17:38 -0700

    KeyWord Search is finished

--- Controller/HousingSearchPage.js
+++ Controller/HousingSearchPage.js
@@ -9,11 +9,17 @@ import House from '../Model/House';
 import User from '../Model/User';
 import RF from "react-native-responsive-fontsize";
 import HousePreviewView from '../View/HousePreviewView';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const Items_Per_Page = 6;
 
 export default class HousingSearchPage extends React.Component{
 	state = {
-		housingItems: null,
-		isFetchingHouseData: true
+		housingItems: [],
+		displayList: [],
+		isFetchingHouseData: true,
+		page: 0,
+		searchQuery: ""
 	}
 
 	constructor() {
@@ -30,18 +36,28 @@ export default class HousingSearchPage extends React.Component{
 	// Get housing data and set state with the new data. 
 	// Can be used on first launch and on refresh request. 
 	getHousingData = () => {
+		console.log("getting all Data");
 		this.setState({
 			isFetchingHouseData: true
 		})
+		const zero = 0;
 		this.housesRef.orderBy("post_date", "desc").get().then(snapshot => {
 			let housingItems = [];
 			snapshot.forEach(house => {
 				var aHouse = new House(house.data(), house.id);
 				housingItems.push(aHouse);
 			});
-			
 			this.setState({
 				housingItems: housingItems,
+				page: zero,
+			});
+			const { page, displayList } = this.state;
+			const start = page*Items_Per_Page;
+			const end = (page+1)*Items_Per_Page-1;
+			var newData = housingItems.slice(start,end);
+			this.setState({
+				displayList:[...displayList,...newData],
+				page:page+1,
 				isFetchingHouseData: false
 			});
 		});
@@ -53,17 +69,67 @@ export default class HousingSearchPage extends React.Component{
 		});
 	}
 
+	loadMore = () => {
+		console.log("load data");
+		if(this.state.housingItems == null)
+		{
+			this.getHousingData();
+		}
+		this.setState({
+			isFetchingHouseData: true
+		})
+		const { page, displayList } = this.state;
+		const start = page*Items_Per_Page;
+		const end = (page+1)*Items_Per_Page-1;
+		console.log("start:" + start);
+		console.log("end" + end);
+		var newData = this.state.housingItems.slice(start,end);
+		this.setState({
+			displayList:[...displayList,...newData],
+			page:page+1,
+			isFetchingHouseData: false
+		});
+	}
+
 	componentWillMount() {
 		this.getHousingData();
 	}
 
 	updateSearchQuery = searchQuery => {
 		this.setState({ searchQuery });
-		this.searchAndUpdateWithQuery(this.state.searchQuery);
 	};
 	
-	searchAndUpdateWithQuery = async (searchQuery) => {
+	searchAndUpdateWithQuery = () => {
+		this.setState({
+			isFetchingHouseData: true
+		})
+		if(this.state.searchQuery == ""){
+			this.getHousingData();
+		}
+		var searchString = this.state.searchQuery.toString().split(" ");
+		console.log(searchString);
+		var bf = require("./bloomfilter"),
+        bloom=bf.BloomFilter;
+		let newHousingItems = [];
+	 	this.state.housingItems.forEach(function(housingItem){
+			let bloomfilterArr = JSON.parse(housingItem.bloomfilter);
+			var Bloom = new bloom(bloomfilterArr,16);
+			for(var i = 0; i < searchString.length; i++){
+				if(Bloom.test(searchString[i])){
+					newHousingItems.push(housingItem);
+					console.log(housingItem.title);
+					break;
+				}
+			}
+			});
+		this.setState({
+				displayList: newHousingItems,
+				isFetchingHouseData: false
+		});
+
+		
 		// Search here with this.houseRef or with Algolia and update housing lists async. 
+
 	}
 
 	render = () => {
@@ -85,12 +151,19 @@ export default class HousingSearchPage extends React.Component{
 						onChangeText={this.updateSearchQuery}
 						value={this.state.searchQuery}
 					/>
+					<TouchableOpacity onPress={this.searchAndUpdateWithQuery}>
+							<View>
+								<Text>Search</Text>
+							</View>
+					</TouchableOpacity>
 				</View>
 				<FlatList
 					keyExtractor={(item, index) => index.toString()}
-					data={this.state.housingItems}
+					data={this.state.displayList}
 					onRefresh={this.getHousingData}
 					refreshing={this.state.isFetchingHouseData}
+					onEndReached={this.loadMore}
+					onEndReachedThreshold={0.7}
 					renderItem={({item}) => (
 							<HousePreviewView
 								house={item}
