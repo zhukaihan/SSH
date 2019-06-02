2b76f55061b6e0228b86a234e0ecac4d83ac6fc0
Peter Zhu <kaihan.zhu@zhukaihan.com>
2019-06-02 06:36:12 -0700

    Styled MessageRoom
    Styled MessageRoom with MessageBubble and TextInput.

--- Controller/MessageRoomView.js
+++ Controller/MessageRoomView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Button, FlatList, TouchableHighlight, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, Button, FlatList, TouchableHighlight, ScrollView, Dimensions, Keyboard } from 'react-native';
 import { Icon, Image } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import firebase from 'firebase';
@@ -13,17 +13,25 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import { TextInput } from 'react-native-gesture-handler';
 import { Message, MessageRoom } from '../Model/Messaging';
 
+// Source: https://github.com/APSL/react-native-keyboard-aware-scroll-view/blob/master/lib/KeyboardAwareHOC.js
+import { isIphoneX } from 'react-native-iphone-x-helper'
+import MessageBubble from '../View/MessageBubble';
+const DEFAULT_TAB_BAR_HEIGHT = isIphoneX() ? 83 : 49
 
 export default class MessageRoomView extends React.Component{
 	state = {
-		room: null
+		room: null,
+		keyboardHeight: 0
 	}
 
 	constructor() {
 		super();
 	}
 
-	componentDidMount = async () => {
+	componentWillMount = async () => {
+		this.keyboardShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardShow)
+		this.keyboardHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardHide)
+		
 		let roomId = this.props.navigation.getParam("roomId", "");
 		if (roomId == "") {
 			return
@@ -47,9 +55,24 @@ export default class MessageRoomView extends React.Component{
 		this.observer();
 
 		this.updateLastReadTime();
+
+		this.keyboardShowListener.remove()
+    this.keyboardHideListener.remove()
 	}
 
-	sendMessage = (txt) => {
+	keyboardShow = (event) => {
+		this.setState({
+			keyboardHeight: event.endCoordinates.height
+		})
+	}
+
+	keyboardHide = (event) => {
+		this.setState({
+			keyboardHeight: 0
+		})
+	}
+
+	sendMessage = async (txt) => {
 		this.roomRef.set({
 			messages: firebase.firestore.FieldValue.arrayUnion({
 				timestamp: firebase.firestore.Timestamp.now(),
@@ -67,7 +90,9 @@ export default class MessageRoomView extends React.Component{
 			}),
 			last_contact_date: firebase.firestore.Timestamp.now(),
 		}, {merge: true})
-		this.newMsgTextInput.clear();
+		this.setState({
+			newMsgText: ""
+		})
 	}
 
 	updateLastReadTime = async () => {
@@ -81,24 +106,73 @@ export default class MessageRoomView extends React.Component{
 			return (<View></View>)
 		}
 
+		var messageViewStyle;
+		if (!this.state.messageViewHeight) {
+			messageViewStyle = {
+				flex: 1
+			}
+		} else {
+			messageViewStyle = {
+				height: this.state.messageViewHeight
+			}
+		}
+
 		return (
-			<SafeAreaView style={{flex: 1}} forceInset={{top: 'never'}}>
-				<KeyboardAwareScrollView>
+			<SafeAreaView style={{flex: 1}}>
+				<View style={{flex: 1}}>
 					<FlatList
+						style={{
+							flex: 1,
+						}}
 						keyExtractor={(item, index) => index.toString()}
 						data={this.state.room.messages}
 						renderItem={({item}) => {
-							return (<Text>{item.timestamp > this.state.room.last_read_time ? "unread" : ""} {item.isSentByUser ? "me: " : "they: "} {item.message}</Text>);
+							return (
+								<MessageBubble isLeft={!item.isSentByUser} text={item.message} />
+								// {item.timestamp > this.state.room.last_read_time ? "unread" : ""}
+							)
 						}}
+						ref={ref => this.flatList = ref}
+						onContentSizeChange={() => this.flatList.scrollToEnd({animated: true})}
+						onLayout={() => this.flatList.scrollToEnd({animated: true})}
 					/>
-					<TextInput
-						style={{borderColor: 'black', borderWidth: 3}}
-						onSubmitEditing={(event) => {
-							this.sendMessage(event.nativeEvent.text)
-						}}
-						ref={input => { this.newMsgTextInput = input }}
-					/>
-				</KeyboardAwareScrollView>
+					<View style={{
+						height: RF(7),
+						width: '100%',
+						backgroundColor: '#eeeeee',
+						justifyContent: 'center',
+					}}>
+						<TextInput
+							style={{
+								height: RF(5),
+								fontSize: RF(2),
+								lineHeight: RF(5),
+								marginLeft: '5%',
+								marginRight: '5%',
+								paddingLeft: 10,
+								paddingRight: 10,
+								width: '90%',
+								borderColor: 'grey', 
+								borderRadius: RF(2),
+								borderWidth: 1,
+								backgroundColor: 'white'
+							}}
+							value={this.state.newMsgText}
+							onChangeText={(text) => this.setState({newMsgText: text})}
+							onSubmitEditing={(event) => {
+								this.newMsgTextInput.focus()
+								this.sendMessage(event.nativeEvent.text)
+							}}
+							blurOnSubmit={false}
+							ref={input => { this.newMsgTextInput = input }}
+						/>
+					</View>
+				</View>
+				{/* Below is a filler view that have the same size as the keyboard */}
+				<View style={{
+					height: this.state.keyboardHeight - DEFAULT_TAB_BAR_HEIGHT
+				}}></View>
+				
       </SafeAreaView>
 		);
 	}
